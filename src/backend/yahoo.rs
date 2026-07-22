3b45cc7195f22079157c8b765c5dc1f6f1eb5619
Gianluca Pozzo <gpozzo@web.de>
2026-07-22 17:15:38 +0200

    Get first financial data from yahoo

---
+++ src/backend/yahoo.rs
@@ -0,0 +1,26 @@
+use anyhow::Result;
+use yahoo_finance_api::{self as yahoo, YResponse};
+
+pub enum TimeFrame {
+    OneDay,
+    OneWeek,
+    OneMonth,
+    ThreeMonth,
+    SixMonth,
+    YearToDate,
+    OneYear,
+    FiveYear,
+    Max,
+}
+
+pub async fn get_asset_data(asset_ticker: String, _time_frame: TimeFrame) -> Result<YResponse> {
+    let provider = yahoo::YahooConnector::new()?;
+
+    let response = provider
+        .get_latest_quotes(asset_ticker.as_str(), "1d")
+        .await?;
+
+    println!("{:?}", response);
+
+    Ok(response)
+}
