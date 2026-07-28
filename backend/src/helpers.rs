1fab10a58681b326b8bbb2610f66b7a586bfc307
Gianluca Pozzo <gpozzo@web.de>
2026-07-28 18:25:54 +0200

    Save

--- backend/src/helpers.rs
+++ backend/src/helpers.rs
@@ -1,16 +1,3 @@
 use crate::types::{Transaction, TransactionType};
 use rust_decimal::Decimal;
 use rust_decimal_macros::dec;
-
-/// Calculates the number of bought and sold shares and returns them as (bought, sold)
-pub fn calc_shares(transactions: &[Transaction], ticker: &str) -> (Decimal, Decimal) {
-    let mut buy = dec!(0);
-    let mut sell = dec!(0);
-    for tx in transactions.iter().filter(|t| t.ticker == ticker) {
-        match tx.transaction_type {
-            TransactionType::Buy => buy = buy + tx.quantity,
-            TransactionType::Sell => sell = sell + tx.quantity,
-        }
-    }
-    (buy, sell)
-}
--- backend/src/service/helpers.rs
+++ backend/src/service/helpers.rs
@@ -69,16 +69,3 @@ pub fn add_dividend_to_list(
     div_list.push(new_dividend);
     Ok(())
 }
-
-/// Calculates the number of bought and sold shares and returns them as (bought, sold)
-pub fn calc_shares(transactions: &[Transaction], ticker: &str) -> (Decimal, Decimal) {
-    let mut buy = dec!(0);
-    let mut sell = dec!(0);
-    for tx in transactions.iter().filter(|t| t.ticker == ticker) {
-        match tx.transaction_type {
-            TransactionType::Buy => buy = buy + tx.quantity,
-            TransactionType::Sell => sell = sell + tx.quantity,
-        }
-    }
-    (buy, sell)
-}
