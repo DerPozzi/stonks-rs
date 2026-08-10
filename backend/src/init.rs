2472557d126c74ce26edbe9f353653daff3c2d22
Gianluca Pozzo <gpozzo@web.de>
2026-08-10 18:53:14 +0200

    Fix database path

--- backend/src/init.rs
+++ backend/src/init.rs
@@ -5,17 +5,8 @@ use rusqlite::Connection;
 
 use crate::database::database::create_tables;
 
-pub fn create_if_no_cfg(config_path: PathBuf) -> Result<()> {
-    if !config_path.exists() {
-        fs::create_dir_all(config_path.parent().unwrap())?;
-    }
-
-    fs::write(config_path, "")?;
-    Ok(())
-}
-
 pub fn open_database(home_path: PathBuf) -> Result<Connection> {
-    let db_path = home_path.join(".stonks-rs");
+    let db_path = home_path.join(".stonks-rs/");
     let db_path = format!("{}stonks-rs.db", db_path.display());
     let conn = Connection::open(db_path)?;
 
