c4b80f1c4a126777dccb4b04fd84291acf16ba36
Gianluca Pozzo <gpozzo@web.de>
2026-08-10 15:02:06 +0200

    Add better Documentation

--- backend/src/types.rs
+++ backend/src/types.rs
@@ -73,34 +73,90 @@ impl FromSql for Currency {
 }
 
 #[derive(Debug, Clone)]
-/// Transaction type holding all informtaion on past transactions
+/// Represents a completed trade of an asset.
+///
+/// A transaction stores the price and monetary values in the currency
+/// specified by [`Transaction::currency`]. This currency does not
+/// necessarily have to be the asset's native/quote currency.
+///
+/// For example, an asset may normally be quoted in USD while a broker
+/// executes and settles the transaction in EUR. In that case, `price`
+/// represents the price per share in EUR and `currency` is `EUR`.
 pub struct Transaction {
     pub id: Option<i64>,
-    /// Ticker of the given asset, make sure to use the right ticker from the right exchange
+
+    /// Ticker symbol identifying the traded asset.
+    ///
+    /// The ticker must be interpreted together with the corresponding
+    /// exchange to uniquely identify the asset.
     pub ticker: String,
-    /// Can either be buy or sell
+
+    /// Type of transaction, e.g. buy or sell.
     pub transaction_type: TransactionType,
+
+    /// Date on which the trade was executed.
     pub trade_date: NaiveDate,
-    /// Amount of shares traded in transaction
+
+    /// Number of shares traded.
     pub quantity: Decimal,
-    /// Price per share
+
+    /// Price paid or received per share.
+    ///
+    /// The price is denominated in [`Transaction::currency`].
+    /// Consequently, `quantity * price` represents the transaction
+    /// value in this currency.
+    ///
+    /// The currency of the price does not necessarily have to match
+    /// the asset's native or quote currency. For example, a USD-denominated
+    /// asset can have a transaction price in EUR if the broker settles
+    /// the trade in EUR.
     pub price: Decimal,
-    /// Currency the transaction was made in, conversion will be done automatically if the assets
-    /// currency differs
+
+    /// Currency in which the transaction price and transaction value
+    /// are denominated.
+    ///
+    /// This is the currency used for the monetary values stored in this
+    /// transaction and does not necessarily represent the asset's
+    /// native/quote currency.
     pub currency: Currency,
+
+    /// Fees charged for the transaction.
+    ///
+    /// Fees are assumed to be denominated in [`Transaction::currency`].
     pub fees: Decimal,
 }
 
 #[derive(Debug, Clone)]
-/// Dividend type holding all informtaion on paid out dividends
+/// Represents a dividend payment received for an asset.
+///
+/// The dividend amount and associated taxes are denominated in the
+/// currency specified by [`Dividend::currency`].
 pub struct Dividend {
     pub id: Option<i64>,
-    /// Ticker of the given asset, make sure to use the right ticker from the right exchange
+
+    /// Ticker symbol identifying the asset that paid the dividend.
+    ///
+    /// The ticker must be interpreted together with the corresponding
+    /// exchange to uniquely identify the asset.
     pub ticker: String,
+
+    /// Date on which the dividend was paid.
     pub payment_date: NaiveDate,
-    /// Amount of paid out
+
+    /// Gross dividend amount paid by the asset.
+    ///
+    /// The amount is denominated in [`Dividend::currency`].
     pub amount: Decimal,
+
+    /// Taxes withheld from the dividend.
+    ///
+    /// The taxes are denominated in [`Dividend::currency`].
     pub taxes: Decimal,
-    /// Currency of the pay out, will be converted automatically
+
+    /// Currency in which the dividend amount and withheld taxes
+    /// are denominated.
+    ///
+    /// This is the currency of the actual dividend payment and does
+    /// not necessarily have to match the asset's native/quote currency.
     pub currency: Currency,
 }
