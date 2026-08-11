4f5fbc1ca714e46a552def1758c2557b0593894e
Gianluca Pozzo <gpozzo@web.de>
2026-08-11 08:13:33 +0200

    fix: #9 Create path for db file if not exist

--- backend/src/init.rs
+++ backend/src/init.rs
@@ -6,8 +6,11 @@ use rusqlite::Connection;
 use crate::database::database::create_tables;
 
 pub fn open_database(home_path: PathBuf) -> Result<Connection> {
-    let db_path = home_path.join(".stonks-rs/");
-    let db_path = format!("{}stonks-rs.db", db_path.display());
+    let db_path = home_path.join(".stonks-rs/").join("stonks-rs.db");
+    if let Some(parent) = db_path.parent() {
+        fs::create_dir_all(parent)?;
+    }
+
     let conn = Connection::open(db_path)?;
 
     create_tables(&conn)?;
