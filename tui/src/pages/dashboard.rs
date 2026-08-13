b6b0799d6a6598d5b006f380f89fedbf2c49a63c
Gianluca Pozzo <gpozzo@web.de>
2026-08-13 13:49:36 +0200

    Add currency to dashboard

--- tui/src/pages/dashboard.rs
+++ tui/src/pages/dashboard.rs
@@ -5,7 +5,11 @@ use crate::app::App;
 pub fn render(app: &App, frame: &mut Frame, area: Rect) {
     // Dashboard rendern
 
-    let portfolio_value = Paragraph::new(format!("{}", app.portfolio_value.round_dp(2)));
+    let portfolio_value = Paragraph::new(format!(
+        "{} {}",
+        app.portfolio_value.round_dp(2),
+        app.settings.default.currency.unwrap_or_default()
+    ));
 
     frame.render_widget(portfolio_value, area);
 }
