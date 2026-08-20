a0703fc925ba8401182592d5acaccc6187c7a3c8
Gianluca Pozzo <gpozzo@web.de>
2026-08-20 11:46:41 +0200

    Display a loading text

--- tui/src/pages/overview.rs
+++ tui/src/pages/overview.rs
@@ -1,33 +1,49 @@
 use ratatui::{
     Frame,
-    layout::Rect,
+    layout::{Alignment, Rect},
     style::Style,
     text::Span,
-    widgets::{Cell, Paragraph, Row},
+    widgets::{Block, BorderType, Borders, Cell, Paragraph, Row, Wrap},
 };
 use rust_decimal_macros::dec;
 
 use crate::{app::App, components::*};
 
-pub fn render(app: &App, frame: &mut Frame, area: Rect) {
+pub fn render(app: &mut App, frame: &mut Frame, area: Rect) {
     let _test = Paragraph::new(format!("{:#?}", app.portfolio.ticker_info));
 
     let mut entries: Vec<_> = app.portfolio.ticker_info.iter().collect();
+
+    if entries.is_empty() {
+        let paragraph =
+            Paragraph::new("No data to display right now.\nIt might still be loading...")
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .border_type(BorderType::Rounded),
+                )
+                .wrap(Wrap { trim: true })
+                .style(Style::default().fg(app.theme.text));
+
+        frame.render_widget(paragraph, area);
+        return;
+    }
 
     entries.sort_by(|(_, a), (_, b)| a.unrealized_gain.cmp(&b.unrealized_gain));
     entries.reverse();
 
     let table_header = vec![
         Cell::from("Name"),
-        Cell::from("Ticker"),
+        // Cell::from("Ticker"),
         Cell::from("Invested"),
         Cell::from("Market Value"),
         Cell::from("Unrealized Gain"),
-        Cell::from("Amount @ price"),
-        Cell::from("Current price"),
+        Cell::from("Amount @ Price"),
+        Cell::from("Current Price"),
     ];
 
-    let table_rows = entries.iter().map(|(t, tx)| {
+    let table_rows = entries.iter().map(|(_t, tx)| {
         let gain_style = Style::default().fg(if tx.unrealized_gain_perc > dec!(0) {
             app.theme.success
         } else if tx.unrealized_gain_perc == dec!(0) {
@@ -41,7 +57,7 @@ pub fn render(app: &App, frame: &mut Frame, area: Rect) {
             } else {
                 tx.name.clone()
             }),
-            Cell::from(t.to_uppercase()),
+            // Cell::from(t.to_uppercase()),
             Cell::from(format!(
                 "{} {}",
                 tx.cost_basis.round_dp(2).to_string(),
